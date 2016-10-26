ba90210149da79f9b5d5d3b01c711c8f6ddd81fd
akandykeller <akandykeller@gmail.com>
2016-10-26 11:51:03 -0700

    Initial commit of auto-encoder decoder branch model

---
+++ training_ae/run_eval_sick.py
@@ -0,0 +1,11 @@
+import tools
+import eval_sick
+
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+if __name__=='__main__':
+    embed_map = tools.load_googlenews_vectors()
+    model = tools.load_model(embed_map)
+
+    eval_sick.evaluate(model, evaltest=True)
